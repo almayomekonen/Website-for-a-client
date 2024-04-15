21103f12a90fcf2abd9a209252bc16ee88b28be9
adiabil <User@almayos-mekonen-MacBook-Air.local>
2024-04-15 10:24:18 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import React from 'react';
 
-export default {
-  plugins: [react()],
-};
+export default defineConfig({
+  base: '/fasika-hair-style/',
+  plugins: [React()],
+  build: {
+    outDir: 'build',
+  },
+});
